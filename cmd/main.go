255ee5a11521ad53bc54399b8b4e46f05bbe6a59
Wyatt Bramblett <wbramblett1@gmail.com>
2025-01-11 22:03:52 -0500

    Started lexer

---
+++ cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/WyattBram/Ine/internal/lexer"
+
+func main() {
+	lexer.Tokenizer()
+}
--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -1,8 +1,31 @@
 package lexer
 
+import (
+	"fmt"
+	"os"
+)
+
 type token struct {
 	Value      string
 	Identifier int16
 }
 
-//func tokenizer()
+func Tokenizer() string {
+	data, err := os.ReadFile("test.ine")
+	if err != nil {
+		return "Problem"
+	}
+
+	tokens := []string{}
+
+	for i := range data {
+		if string(data[i]) == "\n" {
+			continue
+		}
+		tokens = append(tokens, string(data[i]))
+	}
+
+	fmt.Println(tokens)
+
+	return "This works"
+}
