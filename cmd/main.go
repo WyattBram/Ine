49fca0129a00ec09179bd9fda7718ff4921e18ab
Wyatt Bramblett <wbramblett1@gmail.com>
2025-01-25 19:01:27 -0500

    The parser is sort of working now, yay

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "github.com/WyattBram/Ine/internal/parser"
+import (
+	"github.com/WyattBram/Ine/internal/lexer"
+	"github.com/WyattBram/Ine/internal/parser"
+	"github.com/sanity-io/litter"
+)
 
 func main() {
-	parser.TestParse()
+	ast := parser.Parse(lexer.Tokenizer())
+
+	litter.Dump(ast)
 }
